c0996b00f39e1013ab112fe43d34b99e2e99c844
emmpak <eiliev05@yahoo.com>
2017-06-12 13:25:58 +0100

    Add solution to factorialize function

--- src/basicAlgorithms.js
+++ src/basicAlgorithms.js
@@ -3,3 +3,8 @@ var exports = module.exports={};
 exports.reverseString = function(str) {
   return str.split('').reverse().join('');
 };
+
+exports.factorialize = function(num) {
+  if (num === 0) { return 1; }
+  return num * this.factorialize(num-1);
+};
