178bdfe1ec69c97be958b2fb4b99275184585ef6
emmpak <eiliev05@yahoo.com>
2017-06-12 16:52:53 +0100

    Add tests for largestOfFour function

--- spec/basicAlgorithmsSpec.js
+++ spec/basicAlgorithmsSpec.js
@@ -61,3 +61,10 @@ describe('titleCase', function() {
     expect(basic.titleCase("HERE IS MY HANDLE HERE IS MY SPOUT")).toEqual("Here Is My Handle Here Is My Spout");
   });
 });
+
+describe('largestOfFour', function() {
+  it('should return largest number from each provided sub-array', function() {
+    expect(basic.largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]])).toEqual([27,5,39,1001]);
+    expect(basic.largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]])).toEqual([9, 35, 97, 1000000]);
+  });
+});
