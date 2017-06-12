f334c1ef858b81f77f60337d18ac9eabd2fa9b0d
emmpak <eiliev05@yahoo.com>
2017-06-12 11:05:33 +0100

    Add test for factorialize function

--- spec/basicAlgorithmsSpec.js
+++ spec/basicAlgorithmsSpec.js
@@ -9,5 +9,17 @@ describe('reverseString', function() {
     expect(basic.reverseString('hello')).toEqual('olleh');
     expect(basic.reverseString('Howdy')).toEqual('ydwoH');
     expect(basic.reverseString('Greetings from Earth')).toEqual('htraE morf sgniteerG');
-  })
-})
+  });
+});
+
+describe('factorialize', function() {
+  it('returns a number', function() {
+    expect(basic.factorialize(2)).toEqual(jasmine.any(Number));
+  });
+
+  it('returns the product of all positive integers less than or equal to n', function() {
+    expect(basic.factorialize(5)).toEqual(120);
+    expect(basic.factorialize(10)).toEqual(3628800);
+    expect(basic.factorialize(0)).toEqual(1);
+  });
+});
