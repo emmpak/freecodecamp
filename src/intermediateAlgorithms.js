f8597ccb2ada96fb0b02cf1c6135c3ca0844f454
emmpak <eiliev05@yahoo.com>
2017-06-18 08:25:26 +0100

    Add sumAll function

---
+++ src/intermediateAlgorithms.js
@@ -0,0 +1,11 @@
+var exports = module.exports={};
+
+exports.sumAll = function(arr) {
+  arr = arr.sort((num1, num2) => num1 - num2);
+  return addNum(arr[0], arr[1]);
+};
+
+function addNum(num1, num2) {
+  if (num1 === num2) return num2;
+  return num2 + addNum(num1, num2-1)
+}
